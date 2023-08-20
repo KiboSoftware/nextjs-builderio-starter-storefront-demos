1d666ffe9ff7e209f69114d5182b90a852650a68
kibo-kevinwatts <85258296+kibo-kevinwatts@users.noreply.github.com>
2023-08-20 18:41:52 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -25,6 +25,7 @@ module.exports = {
       'totalwine.com',
       'www.totalwine.com',
       'cdn-sb.euw1.kibocommerce.com',
+      'cdn-tp1.euw1.kibocommerce.com'
     ],
     deviceSizes: [
       100, 240, 340, 380, 400, 450, 500, 550, 600, 640, 750, 828, 1080, 1200, 1920, 2048, 3840,
